49998398bc42366fc2f66cacc0f1e01590681fa2
WarrenWang02 <122489768+WarrenWang02@users.noreply.github.com>
2024-11-25 18:08:26 -0500

    2

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -6,14 +6,16 @@ public class playerController : MonoBehaviour
 {
     public float speed;
     public float groundDist;
+    public float jumpForce;
 
     public LayerMask terrainLayer;
     public Rigidbody rb;
     public SpriteRenderer sr;
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
+        rb = gameObject.GetComponent<Rigidbody>(); //gameObject
     }
 
     // Update is called once per frame
@@ -40,6 +42,14 @@ public class playerController : MonoBehaviour
         else if (x != 0 && x < 0)
         {
             sr.flipX = false;
+        }
+
+        isGrounded = Physics.Raycast(transform.position, -Vector3.up, groundDist, terrainLayer);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
         }
     }
 }
